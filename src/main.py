13f7155c3ba035758d9e58e692d7f88ac61d11cb
Arthur Dornelas <arthurdornelas99@gmail>
2021-03-17 23:26:15 -0300

    adicao de leitura de digrafos e ponderados

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from utils.quicksort import quickSort
 from app.Node import Node
 from app.Graph import Graph
-from utils.fileReader import readGraph
+from utils.fileReader import readGraphWithWeight
 import sys
 import time
 import math
@@ -11,42 +11,33 @@ def main(args):
   #   print('------------')
   #   print('Round: ', args[1])
   #   print('------------')
-  # numNodes = 0  
+  # numNodes = 0
   # numEdges = 0
   # graph = Graph()
-  # filename = 'school1'
-  # edges = readGraph("in/{filename}".format(filename=filename))
+  # filename = 'grafo_125'
+  # edges = readGraphWithWeight("in/{filename}".format(filename=filename))
+  # print(edges)
   # for edge in edges:
-  #   graph.addEdgeToGraph(edge[0], edge[1])
-  
-  # startTimeGreedy = time.time()
-  # greedyColors = graph.greedyAlgorithm()
-  # print('Cores Guloso: ', greedyColors) 
-  # endTimeGreedy = time.time()
-  # execTimeGreedy = round((endTimeGreedy - startTimeGreedy)*1000, 3)
-  # print("--- %f milliseconds ---" % execTimeGreedy)
+  #   graph.addEdgeToGraphWithWeight(edge[0], edge[1], edge[2])
+  # graph.printGraphWithWeight()  # Imprime o grafo
 
-  # startTimeBruteForce = time.time()
-  # bruteForceColors = graph.bruteForce()
-  # print('Cores Força bruta: ', bruteForceColors) 
-  # endTimeBruteForce = time.time()
-  # execTimeBruteForce = round((endTimeBruteForce - startTimeBruteForce)*1000, 3)
-  # print("--- %f milliseconds ---" % execTimeBruteForce)
+  # startTimeFloyd = time.time()
+  # # greedyColors = graph.greedyAlgorithm()
+  # # print('Cores Guloso: ', greedyColors)
+  # endTimeFloyd = time.time()
+  # execTimeFloyd = round((endTimeFloyd - startTimeFloyd)*1000, 3)
+  # print("--- %f milliseconds ---" % execTimeFloyd)
 
   # with open('out/{filename}-out.csv'.format(filename=filename), 'a') as outFile:
-  #   outFile.write('{greedyColors};{execTimeGreedy};{bruteForceColors};{execTimeBruteForce} \n'
-  #     .format(
-  #       greedyColors=greedyColors, 
-  #       execTimeGreedy=str(execTimeGreedy).replace('.',','), 
-  #       execTimeBruteForce=str(execTimeBruteForce).replace('.',','), 
-  #       bruteForceColors=bruteForceColors)
-  #   )
+  #   outFile.write('{execTimeFloyd}; \n'.format(execTimeFloyd=str(execTimeFloyd).replace('.', ','))
+
+  # #graph.printGraphWithWeight() # Imprime o grafo
 
   graph = Graph()
   graph.addEdgeToGraphWithWeight(1,2,5)
   graph.addEdgeToGraphWithWeight(1,3,10)
   graph.addEdgeToGraphWithWeight(2,3,15)
-
+  
   graph.printGraphWithWeight() # Imprime o grafo
 
 main(sys.argv)
--- src/utils/fileReader.py
+++ src/utils/fileReader.py
@@ -1,11 +1,11 @@
-def readGraph(filename):
+def readGraphWithWeight(filename):
   with open('{filename}.txt'.format(filename=filename), 'r') as reader:
     lines = reader.readlines()
     edgeArr = []
-    numNodes = lines[0].split(' ')[2]
-    numEdges = lines[0].split(' ')[3].rsplit('\n')[0]
+    numNodes = lines[0].rsplit('\n')[0]
     for index in range(1, len(lines)):
-      node1 = lines[index].split(' ')[1]
-      node2 = lines[index].split(' ')[2].rsplit('\n')[0]
-      edgeArr.append([node1, node2])
+      node1 = lines[index].split(' ')[0]
+      node2 = lines[index].split(' ')[1]
+      weight = lines[index].split(' ')[2].rsplit('\n')[0]
+      edgeArr.append([node1, node2, weight])
     return edgeArr
