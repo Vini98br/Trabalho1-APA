ff72211e8f91844cafac9ea47ce13583d043482d
joaocottabadaro <joaobadaro@gmail.com>
2021-03-18 21:35:32 -0300

    converti a variavel weight para int

--- src/utils/fileReader.py
+++ src/utils/fileReader.py
@@ -6,6 +6,6 @@ def readGraphWithWeight(filename):
     for index in range(1, len(lines)):
       node1 = lines[index].split(' ')[0]
       node2 = lines[index].split(' ')[1]
-      weight = lines[index].split(' ')[2].rsplit('\n')[0]
+      weight = int(lines[index].split(' ')[2].rsplit('\n')[0])
       edgeArr.append([node1, node2, weight])
     return edgeArr
